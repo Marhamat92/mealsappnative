850dff45092c313f71bd99e3ee45388748a93719
Marhamat Baghirov <marhamat.baghirov@makdos.dev>
2022-06-21 11:03:53 +0300

    lottie file added , last commit

--- src/pages/MealsCategories/MealsCategories.js
+++ src/pages/MealsCategories/MealsCategories.js
@@ -1,55 +1,74 @@
-import React ,{useState,useEffect} from 'react'
-import { FlatList, TouchableWithoutFeedback,TouchableOpacity } from 'react-native'
-import { SafeAreaView ,Text, View} from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { FlatList, TouchableWithoutFeedback, TouchableOpacity } from 'react-native'
+import { SafeAreaView, Text, View } from 'react-native'
 import tw from 'twrnc';
 import { getCategoriesList } from '../../apiServices/Services';
 import CategoriesCards from '../../components/MealsCategoriesCards/CategoriesCards';
 import LottieView from 'lottie-react-native';
 
-function MealsCategories({navigation}) {
+function MealsCategories({ navigation }) {
+  const [loading, setLoading] = useState(false);
+  const [categoriesList, setCategoriesList] = useState([]);
 
-const [categoriesList,setCategoriesList] = useState([]);
+  useEffect(() => {
+    
+    setLoading(true);
+    getCategoriesList().then(res => {
+      setCategoriesList(res.categories);
 
-useEffect(() => {
-  getCategoriesList().then(res => {
-  
-    setCategoriesList(res.categories);
-  }).catch(err => {
-    console.log(err);
-  }).finally(() => {
-    console.log('finally');
+      setTimeout(() => {
+        setLoading(false);
+
+      }, 4000);
+
+    }).catch(err => {
+      console.log(err);
+    }).finally(() => {
+      console.log('finally');
+    }
+    )
+  }, []);
+
+
+  function Loading() {
+
+
+    return (<LottieView style={tw`flex items-center bg-white`} source={require('../../assets/Loading/meal3.json')} autoPlay loop
+   
+    
+    />)
+
   }
-  )
-}, []);
-
-
-// function Loading(){
-  
-//    return(<LottieView source={require('../../assets/Loading/mealloading.json')} autoPlay loop />)
-
-// }
 
 
 
   return (
     <SafeAreaView style={tw`bg-yellow-500 w-full h-full flex`}>
-    <FlatList 
-    ItemSeparatorComponent={() => <View style={tw`w-full h-1 bg-yellow-500`}></View>}
-    data={categoriesList}
-    keyExtractor={(item) => item.idCategory}
-    renderItem={({item}) => {
-      return ( 
-      <TouchableOpacity onPress={() => { console.log("hjkhhj")
-        navigation.navigate("Meals",{categoryName:item.strCategory})
-      }}>
-      <CategoriesCards 
-        cardImage={item.strCategoryThumb} 
-        cardName={item.strCategory} 
-        cardId={item.idCategory}
-      /> 
-      </TouchableOpacity>
-      )}}  
-    />
+      {loading ?
+        <Loading />
+        :
+        <FlatList
+          ItemSeparatorComponent={() => <View style={tw`w-full h-1 bg-yellow-500`}></View>}
+          data={categoriesList}
+          keyExtractor={(item) => item.idCategory}
+          renderItem={({ item }) => {
+            return (
+              <TouchableOpacity onPress={() => {
+               
+                navigation.navigate("Meals", { categoryName: item.strCategory })
+              }}>
+                <CategoriesCards
+                  cardImage={item.strCategoryThumb}
+                  cardName={item.strCategory}
+                  cardId={item.idCategory}
+                />
+              </TouchableOpacity>
+            )
+          }}
+        />
+
+
+      }
     </SafeAreaView>
   )
 }
